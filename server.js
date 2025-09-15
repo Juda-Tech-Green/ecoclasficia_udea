2cc34ad8cc5ceaa54a149e52020693763c5bac82
Juda-Tech-Green <juda.techgreen@gmail.com>
2025-09-15 11:52:36 -0500

    fix server config

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get('/juego', (req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+// Redirige la raíz (/) a home.html
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/home.html');
 });
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/home.html')
-})
+// Redirige /juego a index.html
+app.get('/juego', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
+// Ruta comodín para manejar cualquier otra solicitud no encontrada
+app.get('*', (req, res) => {
+    res.status(404).sendFile(__dirname + '/public/404.html');
+});
 
-app.listen(port, ()=>{
-    console.log(`Server started on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
